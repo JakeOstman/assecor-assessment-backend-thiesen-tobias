dd8ef86e4d17a60ca20458d306516377be4a4bc9
Tobias Thiesen <tobridflitzebogen@yahoo.de>
2025-11-26 21:07:01 +0100

    add controller class for person

---
+++ FavoriteColorApi/Controllers/PersonController.cs
@@ -0,0 +1,29 @@
+﻿using FavoriteColorApi.Models;
+using FavoriteColorApi.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace FavoriteColorApi.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class PersonsController : ControllerBase
+    {
+        private readonly PersonService _service;
+
+        public PersonsController(PersonService service)
+        {
+            this._service = service;
+        }
+
+        [HttpGet]
+        public IEnumerable<Person> Get() => this._service.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Person> Get(int id)
+        {
+            var person = this._service.GetById(id);
+            return person is null ? this.NotFound() : this.Ok(person);
+        }
+    }
+
+}
