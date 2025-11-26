bdb6fe595545f04de9d8f5e1b673232660f8691b
Tobias Thiesen <tobridflitzebogen@yahoo.de>
2025-11-26 20:06:18 +0100

    Initial Commit with .gitignore and data model created

---
+++ FavoriteColorApi/DTOs/PersonDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace FavoriteColorApi.DTOs
+{
+    public class PersonDto
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string ZipCode { get; set; }
+        public required string City { get; set; }
+        public required string Color { get; set; }
+    }
+
+}
---
+++ FavoriteColorApi/Models/Person.cs
@@ -0,0 +1,13 @@
+﻿namespace FavoriteColorApi.Models
+{
+    public class Person
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public required string LastName { get; set; }
+        public required string ZipCode { get; set; }
+        public required string City { get; set; }
+        public required string Color { get; set; }
+    }
+
+}
