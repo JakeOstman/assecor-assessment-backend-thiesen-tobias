1caaacabc78be43a14658e17694577cc941ab929
Tobias Thiesen <tobridflitzebogen@yahoo.de>
2025-11-26 21:05:25 +0100

    add CsvDataLoader class

---
+++ FavoriteColorApi/Services/DataLoader/CsvDataLoader.cs
@@ -0,0 +1,28 @@
+﻿namespace FavoriteColorApi.Services.DataLoader
+{
+    using System.Globalization;
+    using System.IO;
+    using CsvHelper;
+    using CsvHelper.Configuration;
+    using FavoriteColorApi.Models;
+
+    public class CsvDataLoader
+    {
+        private readonly string _filePath;
+
+        public CsvDataLoader(string filePath)
+        {
+            this._filePath = filePath;
+        }
+
+        public List<Person> LoadPersons()
+        {
+            using var reader = new StreamReader(this._filePath);
+            using var csv = new CsvReader(reader, CultureInfo.InvariantCulture);
+
+            var records = csv.GetRecords<Person>().ToList();
+            return records;
+        }
+    }
+
+}
