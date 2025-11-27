22c627e699919e7115fb4abb1732bc911c379260
Tobias Thiesen <tobridflitzebogen@yahoo.de>
2025-11-27 23:27:09 +0100

    GetPersons Test extended

--- FavoriteColorApi.Tests/Services/PersonServiceTests.cs
+++ FavoriteColorApi.Tests/Services/PersonServiceTests.cs
@@ -11,16 +11,29 @@ namespace FavoriteColorApi.Tests.Services
     [TestClass]
     public class PersonServiceTests
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void GetPersons_ReturnsExpectedList()
         {
             var service = new PersonService(
-                new CsvDataLoader("m:\\source\\Angular ASP\\Assecor\\assecor-assessment-backend\\FavoriteColorApi\\Data\\sample-input.csv"));
+                new CsvDataLoader("..\\..\\..\\..\\FavoriteColorApi\\Data\\sample-input.csv"));
 
             var result = service.GetAll();
 
             Assert.IsNotNull(result, "The returned list must not be null.");
-            Assert.AreEqual(3, result.Count, "The list should contain exactly 10 items.");
+            Assert.AreEqual(10, result.Count, "The list should contain exactly 10 items.");
+
+            foreach (var person in result)
+            {
+                this.TestContext.WriteLine("Id = " + person.Id.ToString());
+                this.TestContext.WriteLine("LastName = " + person.LastName);
+                this.TestContext.WriteLine("Name = " + person.Name);
+                this.TestContext.WriteLine("ZipCode = " + person.ZipCode);
+                this.TestContext.WriteLine("City = " + person.City);
+                this.TestContext.WriteLine("ColorId = " + person.Color.ToString());
+                this.TestContext.WriteLine(string.Empty);
+            }
         }
     }
 }
