a6cde894b753499e0234305db5ac990d810b9c06
Tobias Thiesen <tobridflitzebogen@yahoo.de>
2025-11-27 14:11:23 +0100

    Add Testproject for unit tests

---
+++ FavoriteColorApi.Tests/Services/PersonServiceTests.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FavoriteColorApi.Services;
+using FavoriteColorApi.Services.DataLoader;
+
+namespace FavoriteColorApi.Tests.Services
+{
+    [TestClass]
+    public class PersonServiceTests
+    {
+        [TestMethod]
+        public void GetPersons_ReturnsExpectedList()
+        {
+            var service = new PersonService(
+                new CsvDataLoader("m:\\source\\Angular ASP\\Assecor\\assecor-assessment-backend\\FavoriteColorApi\\Data\\sample-input.csv"));
+
+            var result = service.GetAll();
+
+            Assert.IsNotNull(result, "The returned list must not be null.");
+            Assert.AreEqual(3, result.Count, "The list should contain exactly 10 items.");
+        }
+    }
+}
