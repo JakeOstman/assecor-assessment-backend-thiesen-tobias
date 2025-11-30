2d0ee4cd31a5af19badc7d0c342cb958212ee12c
JakeOstman <towbiith@yahoo.de>
2025-11-30 11:18:35 +0100

    Added missing Scope for IPersonService

--- FavoriteColorApi/Program.cs
+++ FavoriteColorApi/Program.cs
@@ -13,6 +13,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddScoped<IPersonService, PersonService>();
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
